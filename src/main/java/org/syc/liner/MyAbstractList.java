5ac1f618737fe89d05abfbd366b971bf634c7d15
siyingcheng <283736566@qq.com>
2022-07-21 16:07:09 +0800

    add MyAbstractList

---
+++ src/main/java/org/syc/liner/MyAbstractList.java
@@ -0,0 +1,85 @@
+package org.syc.liner;
+
+import java.util.Objects;
+
+public abstract class MyAbstractList<E> implements IList<E> {
+    protected static final int DEFAULT_CAPACITY = 10;
+
+    protected static final Objects[] EMPTY_ELEMENTS = {};
+
+    /**
+     * Constructs an empty list with the specified initial capacity.
+     *
+     * @param capacity the initial capacity of the list
+     * @throws IllegalArgumentException if the initial capacity is negative
+     */
+    public MyAbstractList(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Illegal capacity:" + capacity);
+        } else {
+            elementData = new Object[capacity];
+        }
+    }
+
+    /**
+     * Constructs an empty list with an initial capacity of {@link #DEFAULT_CAPACITY}
+     */
+    public MyAbstractList() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    /**
+     * The size of the MyArrayList(the number of elements it contains.).
+     */
+    protected int size = 0;
+
+    /**
+     * The array buffer into which the elements of the MyArrayList are stored.
+     */
+    transient Object[] elementData;
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public boolean contains(E e) {
+        return indexOf(e) >= 0;
+    }
+
+    protected void checkIndexForAdd(int index) {
+        Objects.checkIndex(index, size + 1);
+    }
+
+    /**
+     * Checks if the index is within the bounds of the range from 0(inclusive) to {@link #size}(exclusive).
+     *
+     * @param index the index
+     * @throws IndexOutOfBoundsException if the index out of bounds
+     */
+    protected void checkIndex(int index) {
+        Objects.checkIndex(index, size);
+    }
+
+    @Override
+    public int indexOf(E e) {
+        for (int i = 0; i < elementData.length; i++) {
+            if (e == null) {
+                if (elementData[i] == null) {
+                    return i;
+                }
+            } else {
+                if (e.equals(elementData[i])) {
+                    return i;
+                }
+            }
+        }
+        return ELEMENT_NOT_FOUND;
+    }
+}
--- src/main/java/org/syc/liner/MyArrayList.java
+++ src/main/java/org/syc/liner/MyArrayList.java
@@ -1,58 +1,8 @@
 package org.syc.liner;
 
 import java.util.Arrays;
-import java.util.Objects;
 
-public class MyArrayList<E> implements IList<E> {
-    private static final int DEFAULT_CAPACITY = 10;
-
-    private static final Objects[] EMPTY_ELEMENTS = {};
-
-    /**
-     * The size of the MyArrayList(the number of elements it contains.).
-     */
-    private int size = 0;
-
-    /**
-     * The array buffer into which the elements of the MyArrayList are stored.
-     */
-    transient Object[] elementData;
-
-    /**
-     * Constructs an empty list with the specified initial capacity.
-     *
-     * @param capacity the initial capacity of the list
-     * @throws IllegalArgumentException if the initial capacity is negative
-     */
-    public MyArrayList(int capacity) {
-        if (capacity < 0) {
-            throw new IllegalArgumentException("Illegal capacity:" + capacity);
-        } else {
-            elementData = new Object[capacity];
-        }
-    }
-
-    /**
-     * Constructs an empty list with an initial capacity of {@link #DEFAULT_CAPACITY}
-     */
-    public MyArrayList() {
-        this(DEFAULT_CAPACITY);
-    }
-
-    @Override
-    public int size() {
-        return size;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        return size == 0;
-    }
-
-    @Override
-    public boolean contains(E e) {
-        return indexOf(e) >= 0;
-    }
+public class MyArrayList<E> extends MyAbstractList<E> {
 
     @Override
     public void add(E e) {
@@ -92,19 +42,6 @@ public class MyArrayList<E> implements IList<E> {
         size++;
     }
 
-    private void checkIndexForAdd(int index) {
-        Objects.checkIndex(index, size + 1);
-    }
-
-    /**
-     * Checks if the index is within the bounds of the range from 0(inclusive) to {@link #size}(exclusive).
-     * @param index the index
-     * @throws IndexOutOfBoundsException if the index out of bounds
-     */
-    private void checkIndex(int index) {
-        Objects.checkIndex(index, size);
-    }
-
     @Override
     public E get(int index) {
         checkIndex(index);
@@ -132,21 +69,6 @@ public class MyArrayList<E> implements IList<E> {
         return oldElement;
     }
 
-    @Override
-    public int indexOf(E e) {
-        for (int i = 0; i < elementData.length; i++) {
-            if (e == null) {
-                if (elementData[i] == null) {
-                    return i;
-                }
-            } else {
-                if (e.equals(elementData[i])) {
-                    return i;
-                }
-            }
-        }
-        return ELEMENT_NOT_FOUND;
-    }
 
     @Override
     public void clear() {
